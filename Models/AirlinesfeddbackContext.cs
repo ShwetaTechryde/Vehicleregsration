b8557af512183b14fc870886dd1f83f4b8b3bbce
Dell <sweta18singh@gmail.com>
2024-12-26 09:29:37 -0800

    sweta

---
+++ Models/AirlinesfeddbackContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Vehicleregsration.Models;
+
+public partial class AirlinesfeddbackContext : Microsoft.EntityFrameworkCore.DbContext
+{
+    public AirlinesfeddbackContext(DbContextOptions<AirlinesfeddbackContext> options)
+    {
+    }
+
+    public virtual DbSet<Vehichledatum> Vehichledata { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-APMVBAH;\n Initial Catalog=Airlinesfeddback;Integrated Security=True;multipleactiveresultsets=True;encrypt=False");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Vehichledatum>(entity =>
+        {
+            entity.HasKey(e => e.VehId);
+
+            entity.Property(e => e.VehId).HasColumnName("vehId");
+            entity.Property(e => e.VehSerialno).HasColumnName("vehSerialno");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.EntityFrameworkCore;
+using Vehicleregsration.DbContext;
+using Vehicleregsration.Models;
+// Configure DbContext
+var optionsBuilder = new DbContextOptionsBuilder<AirlinesfeddbackContext>();
+optionsBuilder.UseSqlServer("YourConnectionStringHere"); // Replace with your actual connection string
+
+var context = new AirlinesfeddbackContext(optionsBuilder.Options);
+var vehicleOps = new VehicleRegistrationOps(context);
+
+Console.WriteLine("Vehicle Registration CRUD Operations\n");
+
+while (true)
+{
+    Console.WriteLine("Choose an option:");
+    Console.WriteLine("1. Add Product");
+    Console.WriteLine("2. View Product by ID");
+    Console.WriteLine("3. View All Products");
+    Console.WriteLine("4. Update Product");
+    Console.WriteLine("5. Delete Product");
+    Console.WriteLine("6. Exit");
+
+    var choice = Console.ReadLine();
+
+    switch (choice)
+    {
+        
+        case "1":
+            Console.WriteLine("Enter product ID to delete:");
+            var deleteId = int.Parse(Console.ReadLine());
+            vehicleOps.DeleteProduct(deleteId);
+            Console.WriteLine("Product deleted successfully!");
+            break;
+
+        case "6":
+            return;
+
+        default:
+            Console.WriteLine("Invalid option.");
+            break;
+    }
+}
